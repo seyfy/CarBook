c3f4f3a3162c71f55ebba20031334222fb2132aa
seyfy <yalcinseyfullah905@gmail.com>
2026-05-04 21:50:34 +0300

    Admin Tarafındaki işlemler yapıldı istatistik öncesi son ders

---
+++ Frontends/CarBook.Dto/LocationDtos/UpdateLocationDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Dto.LocationDtos
+{
+    public class UpdateLocationDto
+    {
+        public int LocationID { get; set; }
+        public string Name { get; set; }
+
+    }
+}
