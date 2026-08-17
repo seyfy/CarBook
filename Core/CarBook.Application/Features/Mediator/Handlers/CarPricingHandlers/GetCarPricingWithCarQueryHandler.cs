12506ac7f1e81f8e89e3a49e4e8b2e5a7e8568dc
seyfy <yalcinseyfullah905@gmail.com>
2026-08-17 16:40:53 +0300

    Rezervasyon İşlemleri Tamamlandı

--- Core/CarBook.Application/Features/Mediator/Handlers/CarPricingHandlers/GetCarPricingWithCarQueryHandler.cs
+++ Core/CarBook.Application/Features/Mediator/Handlers/CarPricingHandlers/GetCarPricingWithCarQueryHandler.cs
@@ -29,7 +29,8 @@ namespace CarBook.Application.Features.Mediator.Handlers.CarPricingHandlers
                Brand=x.Car.Brand.Name,
                CarPricingId=x.CarPricingID,
                CoverImageUrl=x.Car.CoverImageUrl,
-               Model=x.Car.Model
+               Model=x.Car.Model,
+               CarId = x.CarID
                
             }).ToList();
         }
